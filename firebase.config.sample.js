6b3fbbdf03c0ab660bbeede5bb4e3b7a75052965
ARI\Julien.Chambers <julien.chambers@leadventure.com>
2023-02-09 07:42:51 -0800

    add dummy config and gitignore config

---
+++ firebase.config.sample.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+export const db = getFirestore()
