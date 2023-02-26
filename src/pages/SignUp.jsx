c4a41d6c550d7c919503e38fa06f04fd76390511
ARI\Julien.Chambers <julien.chambers@leadventure.com>
2023-02-26 08:49:38 -0800

    add user signup function

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config' 
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -23,6 +25,19 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user
+      updateProfile(auth.currentUser, {displayName: name})
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -31,7 +46,7 @@ function SignUp() {
             Welcome back!
           </p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
         <input 
             type="text" 
             className="nameInput" 
@@ -74,7 +89,7 @@ function SignUp() {
             <p className="signUpText">
               Sign Up
             </p>
-            <button className="signUpButton">
+            <button className="signUpButton" type="submit">
               <ArrowRightIcon 
                 fill='#ffffff'
                 width='36px'
