ba416c13f011a16b50e95e41e5825d4dba3b30c8
pdxchambers <pdxchambers@gmail.com>
2023-04-08 08:58:32 -0700

    add contact page and listing slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
-import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -10,7 +10,9 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Category from './pages/Category'
 import ForgotPassword from './pages/ForgotPassword'
-import CreateListing from './pages/CreateListing';
+import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
