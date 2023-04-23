9519e6a79274e0cd31dd1fa567fdd27908ffc8c1
pdxchambers <pdxchambers@gmail.com>
2023-04-23 07:21:23 -0700

    update firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGE_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
